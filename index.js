b3ec0d3b4e7ff056014674421ae3cccaec426596
metastasio <myaldzina.m@gmail.com>
2024-06-07 00:49:59 +0300

    Fixed dropdown menu

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const hamButton = document.querySelector('.burger_menu_button');
 const offScreenMenu = document.querySelector('.drop_down_menu');
+const sliderPrew = document.querySelector('.rent_brands_slider_left_arrow');
+const sliderNext = document.querySelector('.rent_brands_slider_right_arrow');
 
 hamButton.addEventListener('click', () => {
   offScreenMenu.classList.toggle('active');
   hamButton.classList.toggle('close');
 });
+
